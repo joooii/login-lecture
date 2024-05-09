ab14704818f4ead1ef9a3bafa2263023866b384f
김주희 <wngml3098@gmail.com>
2024-05-09 14:27:00 +0900

    front 연결

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");    // ("파일명","폴더명")
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);  // use -> 미들 웨어를 등록해주는 메서드
 
